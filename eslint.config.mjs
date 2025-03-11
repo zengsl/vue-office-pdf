9cb480e2c9580826f8a8c8f6fbdef85f842e6731
zengsl <zengsl0212@foxmail.com>
2025-03-11 19:06:26 +0800

    chore:init

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+import {FlatCompat} from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+export default antfu({
+    "rules": {
+        "curly": "off",
+        "no-console": "off",
+        "vue/component-tags-order": ["error", {
+            "order": [["script", "template"], "style"]
+        }]
+    },
+    ...compat.config({
+        extends: [
+            'plugin:case-police/recommended',
+            // Other extends...
+        ],
+    })
+})
